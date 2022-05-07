499406d28f620ef6bf5506f106a48920e79a7945
Samuel Sousa <samuelsousa064@gmail.com>
2022-05-07 13:13:36 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+
+
+var name = Console.ReadLine();
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {DateTime.Now:d} at {DateTime.Now:t}!");
+
+
