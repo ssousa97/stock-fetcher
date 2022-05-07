ba4ca807a01b578113f6d88c3fe4df56cc6e03e4
Samuel Sousa <samuelsousa064@gmail.com>
2022-05-07 15:24:31 -0300

    subing arquivos pro repositorio remoto

--- Program.cs
+++ Program.cs
@@ -1,29 +1,29 @@
-﻿using System.Configuration;
-using System.Globalization;
+﻿using System.Globalization;
 
 class Program {
     static void Main(string[] args){
-       
-        if(args.Length < 3){
-            Console.WriteLine("Faltando argumentos, padrão : <ATIVO> <VALOR DE VENDA> <VALOR DE COMPRA>");
-            return;
-        }
 
-        if(args[1].Contains(',') || args[2].Contains(',')){
-            Console.WriteLine("Valores decimais precisam ser separados por pontos.");
-            return;
-        }
+        try {
 
-        var stock     = args[0];
-        var sellPrice = float.Parse(args[1], CultureInfo.InvariantCulture);
-        var buyPrice  = float.Parse(args[2], CultureInfo.InvariantCulture);
+            if(args.Length < 3){
+                throw new Exception("Faltando argumentos, padrão : <ATIVO> <VALOR DE VENDA> <VALOR DE COMPRA>");
+            }
 
-        if(buyPrice > sellPrice){
-            Console.WriteLine("Valor de compra maior do que valor de venda.");
-            return;
-        }
+            if(args[1].Contains(',') || args[2].Contains(',')){
+                throw new Exception("Valores decimais precisam ser separados por pontos.");
+            }
 
-        
+            var stock     = args[0];
+            var sellPrice = float.Parse(args[1], CultureInfo.InvariantCulture.NumberFormat);
+            var buyPrice  = float.Parse(args[2], CultureInfo.InvariantCulture.NumberFormat);
+
+            if(buyPrice > sellPrice){
+                throw new Exception("Valor de compra maior do que valor de venda.");
+            }
+            
+        } catch(Exception e){
+            Console.Error.WriteLine(e.Message);
+        } 
 
     }
 }
