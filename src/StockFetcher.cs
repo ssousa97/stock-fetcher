091be1d48b5498b30675c8284fb420ad2e829f6d
Samuel Sousa <samuelsousa064@gmail.com>
2022-05-08 21:44:06 -0300

    DTO de stock prices para response da API

--- src/StockFetcher.cs
+++ src/StockFetcher.cs
@@ -1,3 +1,21 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+struct StockPrice {
+    
+
+    [JsonPropertyName("closPric")]
+    public float Price {get; set;}
+
+    [JsonPropertyName("prcFlcn")]
+    public float PriceFluctuation {get; set;}
+
+    [JsonPropertyName("dtTm")]
+    public string Time {get; set;}
+    
+}
 class StockFetcher {
     private string Stock;
     private float SellPrice;
